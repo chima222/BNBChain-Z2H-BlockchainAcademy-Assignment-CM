423bacb92f4e98557e26958002f22d1c31faadf4
chima222 <manchy0409@gmail.com>
2023-03-22 01:02:11 +0800

    fix: remove api key

--- hw1/hardhat-erc20/hardhat.config.js
+++ hw1/hardhat-erc20/hardhat.config.js
@@ -4,10 +4,8 @@ require("@nomicfoundation/hardhat-toolbox")
 require("@nomiclabs/hardhat-etherscan")
 require("hardhat-deploy")
 const PRIVATE_KEY = process.env.PRIVATE_KEY
-const BSCSCAN_API_KEY = process.env.BSCSCAN_API_KEY || "AQI6WTF9KWFMPIHR93CMH4EH9GNGAXBTME"
-const BSCTEST_RPC_URL =
-    process.env.BSCTEST_RPC_URL ||
-    "https://attentive-warmhearted-asphalt.bsc-testnet.discover.quiknode.pro/9b449876b99ae4814f9e5bda2e257c07313daebf"
+const BSCSCAN_API_KEY = process.env.BSCSCAN_API_KEY
+const BSCTEST_RPC_URL = process.env.BSCTEST_RPC_URL
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
